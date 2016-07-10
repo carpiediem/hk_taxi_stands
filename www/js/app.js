5bd0111d594b65c24b543c3e914279806e812f65
Ryan Carpenter <carpiediem@gmail.com>
2016-07-10 18:39:35 +0800

    fixed directions, app store, & email buttons

--- www/js/app.js
+++ www/js/app.js
@@ -17,7 +17,7 @@ var app = angular.module('hk_taxi_stands', ['ionic', 'ngCordova', 'uiGmapgoogle-
       $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
           cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
         if (window.StatusBar) {
